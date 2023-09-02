187fd0726af8a2c5e23ebe327321ceb4e979751a
Schubi222 <73842560+Schubi222@users.noreply.github.com>
2023-09-02 19:11:32 +0200

    added vuetify | started refactoring to vuetify

--- TimeTracker/src/views/HomeView.vue
+++ TimeTracker/src/views/HomeView.vue
@@ -1,28 +1,43 @@
 <template>
-  <div class="Inputs">
-    <form @submit.prevent="validateForm">
-      <div class="input-info-wrapper">
-        <label for="time"></label>
-        <div class="error" id="time_error" v-if="timeError">{{timeError}}</div>
-      </div>
-      <input type="time" class="time" id="time" name="time"
-             v-model="time" min="00:01" required>
 
-      <div class="input-info-wrapper">
-        <label for="cause"></label>
-      </div>
-      <input type="text" class="cause" id="cause" name="cause" v-model="cause">
+    <v-card width="300" class="mx-auto">
+      <v-card-item>
+        <v-form @submit.prevent="validateForm" ref="form">
+          <v-text-field
+              type="time"
+              id="time"
+              name="time"
+              v-model="time"
+              :rules="timeValidation"
+              label="Spent time">
+          </v-text-field>
+
+          <v-text-field
+              type="text"
+              id="cause"
+              name="cause"
+              v-model="cause"
+              label="Additional Information"
+          ></v-text-field>
+
+          <v-select
+              id="select"
+              name="select"
+              label="Select category:"
+              v-model="category"
+              :items="categories"
+              chips
+              required
+          >
+          </v-select>
+
+          <v-btn type="submit" block class="mt-2">Submit</v-btn>
+        </v-form>
+      </v-card-item>
+    </v-card>
 
 
-      <label for="select">Select category:</label>
-      <select id="select" name="select" v-model="category" required>
-        <option v-for="category in categories" :key="category">{{category}}</option>
-      </select>
-      <input type="submit" value="Enter">
-    </form>
 
-    Inputs
-  </div>
   <div class="Display-Stats">Display-Stats</div>
   <div class="All-Entries">All-Entries</div>
 </template>
@@ -32,21 +47,26 @@
   import Entry from "@/types/Entry";
 
   import data from '@/assets/dataBase/data.json';
+  import type {VForm} from "vuetify/components";
 
   const categories = ref(['Dev', 'Total'])
   const time = ref('00:00')
   const cause = ref('')
   const category = ref(categories.value[0])
 
+  const form = ref<undefined|VForm>()
 
-  const timeError = ref('')
 
-  const validateForm = (e: Event) =>{
-    if ( !categories.value.includes(category.value) || time.value === '00:00' || time.value === ''){
-      return
+  const timeValidation = [
+    (time:string) => time !== '00:00' || "Enter a correct time",
+    (time:string) => time !== '' || "Enter a correct time",
+  ]
+
+  const validateForm = async () =>{
+    const valid = await form.value?.validate()
+    if(valid?.valid){
+      console.log("a")
     }
-    const entry = new Entry(time.value, category.value, cause.value);
-    console.log(data)
   }
 </script>
 
